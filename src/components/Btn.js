8394203b1c895fbd60ca9011884bd151ac19b433
tester <aiforeveryoneai@gmail.com>
2020-11-06 11:35:47 +0500

    Final commit

---
+++ src/components/Btn.js
@@ -0,0 +1,22 @@
+function Btn({start, status, stop, reset, resume}) {
+  return (
+    <div>
+      {status === 0 ? 
+      <button className="stopwatch-btn stopwatch-btn-gre" onClick={start}>Start</button>: ""}
+      {status === 1 ?
+      <div>
+          <button className="stopwatch-btn stopwatch-btn-red" onClick={stop}>Stop</button>
+          <button className="stopwatch-btn stopwatch-btn-yel" onClick={reset}>Reset</button>
+      </div>
+       : ""}
+       {status === 2 ?
+      <div>
+          <button className="stopwatch-btn stopwatch-btn-gre" onClick={resume}>Resume</button>
+          <button className="stopwatch-btn stopwatch-btn-yel" onClick={reset}>Reset</button>
+      </div>
+       : ""}
+    </div>
+  );
+}
+
+export default Btn;
